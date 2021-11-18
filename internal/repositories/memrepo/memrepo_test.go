aab7baca19109a5f1f6c91c563eb9eeb152c8d7b
Serg_Kotovsky <rookulab@mail.ru>
2021-11-18 18:29:26 +0300

    mem repo test

--- internal/repositories/memrepo/memrepo_test.go
+++ internal/repositories/memrepo/memrepo_test.go
@@ -7,7 +7,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func GetClonesTest(t *testing.T) {
+func TestGetClones(t *testing.T) {
+
 	fileSlice := []domain.File{
 		{Dir: ".", Name: "clone1", Path: "./clone1", SizeInBytes: 11, CloneID: 0},
 		{Dir: ".", Name: "clone2", Path: "./clone2", SizeInBytes: 12, CloneID: 0},
@@ -24,21 +25,40 @@ func GetClonesTest(t *testing.T) {
 		{Dir: "test-folder3/test-folder4", Name: "clone2", Path: "test-folder3/test-folder4/clone2", SizeInBytes: 12, CloneID: 0},
 		{Dir: "test-folder3/test-folder4", Name: "clone3", Path: "test-folder3/test-folder4/clone3", SizeInBytes: 13, CloneID: 0},
 	}
-	expected := []domain.File{
-		{Dir: ".", Name: "clone1", Path: "./clone1", SizeInBytes: 11, CloneID: 0},
-		{Dir: ".", Name: "clone2", Path: "./clone2", SizeInBytes: 12, CloneID: 0},
-		{Dir: "test-folder1", Name: "clone1", Path: "test-folder1/clone1", SizeInBytes: 11, CloneID: 0},
-		{Dir: "test-folder1", Name: "clone2", Path: "test-folder1/clone2", SizeInBytes: 12, CloneID: 0},
-		{Dir: "test-folder2", Name: "clone1", Path: "test-folder2/clone1", SizeInBytes: 11, CloneID: 0},
-		{Dir: "test-folder2", Name: "clone3", Path: "test-folder2/clone3", SizeInBytes: 13, CloneID: 0},
-		{Dir: "test-folder3", Name: "clone1", Path: "test-folder3/clone1", SizeInBytes: 11, CloneID: 0},
-		{Dir: "test-folder3", Name: "clone2", Path: "test-folder3/clone2", SizeInBytes: 12, CloneID: 0},
-		{Dir: "test-folder3/test-folder4", Name: "clone2", Path: "test-folder3/test-folder4/clone2", SizeInBytes: 12, CloneID: 0},
-		{Dir: "test-folder3/test-folder4", Name: "clone3", Path: "test-folder3/test-folder4/clone3", SizeInBytes: 13, CloneID: 0},
+
+	type simpleFile struct {
+		Name        string
+		SizeInBytes int
+		Path        string
+		Dir         string
+	}
+
+	expected := []simpleFile{
+		{Dir: ".", Name: "clone1", Path: "./clone1", SizeInBytes: 11},
+		{Dir: ".", Name: "clone2", Path: "./clone2", SizeInBytes: 12},
+		{Dir: "test-folder1", Name: "clone1", Path: "test-folder1/clone1", SizeInBytes: 11},
+		{Dir: "test-folder1", Name: "clone2", Path: "test-folder1/clone2", SizeInBytes: 12},
+		{Dir: "test-folder2", Name: "clone1", Path: "test-folder2/clone1", SizeInBytes: 11},
+		{Dir: "test-folder2", Name: "clone3", Path: "test-folder2/clone3", SizeInBytes: 13},
+		{Dir: "test-folder3", Name: "clone1", Path: "test-folder3/clone1", SizeInBytes: 11},
+		{Dir: "test-folder3", Name: "clone2", Path: "test-folder3/clone2", SizeInBytes: 12},
+		{Dir: "test-folder3/test-folder4", Name: "clone2", Path: "test-folder3/test-folder4/clone2", SizeInBytes: 12},
+		{Dir: "test-folder3/test-folder4", Name: "clone3", Path: "test-folder3/test-folder4/clone3", SizeInBytes: 13},
 	}
 
 	memRepo := New()
 	memRepo.StoreFiles(fileSlice)
 	memRepo.GetClones()
-	assert.ElementsMatch(t, memRepo.fileSlice, expected, "Incorrect GetClones result: slices are not equal")
+
+	var outSlice []simpleFile
+	for _, f := range memRepo.fileSlice {
+		fData := simpleFile{
+			Name:        f.Name,
+			Dir:         f.Dir,
+			Path:        f.Path,
+			SizeInBytes: f.SizeInBytes,
+		}
+		outSlice = append(outSlice, fData)
+	}
+	assert.ElementsMatch(t, outSlice, expected, "Incorrect GetClones result: slices are not equal")
 }
