d2ccdc75681afa3cfbdb1ab697884ea3f28c3301
Serg_Kotovsky <rookulab@mail.ru>
2021-11-19 17:01:12 +0300

    add config, add zap

--- cmd/cloremover/main.go
+++ cmd/cloremover/main.go
@@ -2,27 +2,45 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/seggga/practice/internal/filesystem"
 	"github.com/seggga/practice/internal/repositories/memrepo"
 	"github.com/seggga/practice/internal/services/cloremover"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
-	// define filesystem
-	dir, err := os.Getwd()
+	// read config
+	config, err := ReadConfig()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("error reading config, program exit")
 		return
 	}
-	fs := filesystem.New(os.DirFS(dir))
+
+	// init logger
+	loggerConfig, err := newLoggerConfig(*config)
+	if err != nil {
+		fmt.Printf("error reading config, %v", err)
+	}
+	logger, _ := loggerConfig.Build()
+	// logger := zap.New(zapcore.NewCore(zap.Encode))
+	logger.Info("hello, log!")
+	// define filesystem
+	if config.Dir == "" {
+		fmt.Println("directory (Dir) not set in config.yaml, program exit")
+		return
+	}
+	fs := filesystem.New(os.DirFS(config.Dir))
 	// define storage
 	stor := memrepo.New()
 	service := cloremover.New(fs, stor)
 
 	// obtain files
-	err = service.FindFiles(dir)
+	err = service.FindFiles(config.Dir)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -33,11 +51,51 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-
-	// clones removal
+	// remove clones
 	err = service.RemoveClones()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+}
+
+type Config struct {
+	LogLevel string `yaml:"loglevel"`
+	Cores    int    `yaml:"cores"`
+	Dir      string `yaml:"dir"`
+}
+
+// ReadConfig implements filling config from yaml-file
+func ReadConfig() (*Config, error) {
+	// read config file
+	configData, err := ioutil.ReadFile("./configs/config.yaml")
+	if err != nil {
+		return nil, err
+	}
+	// decode config
+	cfg := new(Config)
+	err = yaml.Unmarshal(configData, cfg)
+	if err != nil {
+		return nil, err
+	}
+	return cfg, nil
+}
+
+func newLoggerConfig(config Config) (*zap.Config, error) {
+	var level zap.AtomicLevel
+	switch config.LogLevel {
+	case "debug":
+		level = zap.NewAtomicLevelAt(zapcore.DebugLevel)
+	case "info":
+		level = zap.NewAtomicLevelAt(zapcore.InfoLevel)
+	case "error":
+		level = zap.NewAtomicLevelAt(zapcore.ErrorLevel)
+	default:
+		return nil, fmt.Errorf("incorrect loglevel value")
+	}
+	return &zap.Config{
+		Encoding:    "json",
+		Level:       level,
+		OutputPaths: []string{"stdout"},
+	}, nil
 }
