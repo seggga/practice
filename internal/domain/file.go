e9ace93014b52bb1638091aa39cd9799ed954854
Serg_Kotovsky <rookulab@mail.ru>
2021-11-20 00:48:22 +0300

    add comments

--- internal/domain/file.go
+++ internal/domain/file.go
@@ -2,11 +2,10 @@ package domain
 
 // File represents data about given file
 type File struct {
-	Name        string
-	SizeInBytes int
-	Path        string
-	Dir         string
-	CloneID     int
+	Name        string // file name
+	SizeInBytes int    // file size
+	Path        string // path from parent directory to file name
+	Dir         string // directory path from from parent
+	CloneID     int    // a field to obtain clones
 	// Hash    string
-
 }
