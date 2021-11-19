edb8e174966e077817ff5896f5214109562dd125
Serg_Kotovsky <rookulab@mail.ru>
2021-11-20 00:01:53 +0300

    change RemoveClones

--- internal/services/cloremover/service.go
+++ internal/services/cloremover/service.go
@@ -46,6 +46,7 @@ func (srv *service) FindFiles(path string) error {
 // LeaveClones filters out all unique files from slice
 func (srv *service) GetClones() error {
 	srv.storage.GetClones()
+	srv.storage.GetDeletable()
 	return nil
 }
 
@@ -67,42 +68,21 @@ func sortByPath(sl []domain.File) []domain.File {
 
 // RemoveCLones calls file removal and corresponding storage data change
 func (srv *service) RemoveClones() error {
-
-	clones := srv.storage.ReadFiles()
-	clones = sortByID(clones)
-
-	var cloneID = 0
-	cloneGroup := make([]domain.File, 0)
-	for i, fileData := range clones {
-		// the last element in slice
-		if i == len(clones)-1 {
-			cloneGroup = append(cloneGroup, fileData)
-			cloneID = 0
-		}
-		if fileData.CloneID != cloneID {
-			// new group of clones
-			if len(cloneGroup) != 0 {
-				// cloneGroup contains all identical files
-				// delete all files except the first (with the smallest path)
-				cloneGroup = sortByPath(cloneGroup)
-				for i := 1; i < len(cloneGroup); i += 1 {
-					// remove file from file system
-					if err := srv.fs.RemoveFile(cloneGroup[i]); err != nil {
-						// log
-						return err
-					}
-					// remove data about deleted file from the storage
-					srv.storage.RemoveFile(cloneGroup[i])
-					// log
-				}
-				// clear cloneGroup
-				cloneGroup = cloneGroup[:0]
-				continue
-			}
-			// create a new slice of clones
-			cloneID = fileData.CloneID
-		}
-		cloneGroup = append(cloneGroup, fileData)
+	err := srv.storage.GetClones()
+	if err != nil {
+		srv.slogger.Errorf("error geting clones, %v", err)
+		return err
 	}
+	srv.storage.GetDeletable()
+	clones := srv.storage.ReadFiles()
+
+	for _, file := range clones {
+		// remove file from file system
+		if err := srv.fs.RemoveFile(file); err != nil {
+			return err
+		}
+	}
+	srv.storage.StoreFiles(clones[:0])
+	srv.slogger.Debug("clones has been deleted")
 	return nil
 }
