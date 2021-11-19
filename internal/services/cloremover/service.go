dd495b42076f3147aa27d30141b13ae500e11648
Serg_Kotovsky <rookulab@mail.ru>
2021-11-20 00:49:18 +0300

    remove unused functio

--- internal/services/cloremover/service.go
+++ internal/services/cloremover/service.go
@@ -1,31 +1,26 @@
 package cloremover
 
 import (
-	"sort"
-
-	"github.com/seggga/practice/internal/domain"
 	"github.com/seggga/practice/internal/ports"
 	"go.uber.org/zap"
 )
 
-type service struct {
+type Service struct {
 	fs      ports.FileSystem
 	storage ports.Storager
 	dir     string
 	slogger *zap.SugaredLogger
-	// mutex sync.Mutex
 }
 
-func New(fs ports.FileSystem, stor ports.Storager, slogger *zap.SugaredLogger) *service {
-	return &service{
+func New(fs ports.FileSystem, stor ports.Storager, slogger *zap.SugaredLogger) ports.RemoverService {
+	return &Service{
 		fs:      fs,
 		storage: stor,
 		slogger: slogger,
-		// mutex: sync.Mutex{},
 	}
 }
 
-func (srv *service) FindFiles(path string) error {
+func (srv *Service) FindFiles(path string) error {
 	// obtain all the subfolders in the given path
 	dirSlice, err := srv.fs.FindSubfolders(path)
 	if err != nil {
@@ -44,30 +39,14 @@ func (srv *service) FindFiles(path string) error {
 }
 
 // LeaveClones filters out all unique files from slice
-func (srv *service) GetClones() error {
+func (srv *Service) GetClones() error {
 	srv.storage.GetClones()
 	srv.storage.GetDeletable()
 	return nil
 }
 
-// sortFiles sorts by CloneID
-func sortByID(sl []domain.File) []domain.File {
-	sort.Slice(sl, func(i, j int) bool {
-		return sl[i].CloneID < sl[j].CloneID
-	})
-	return sl
-}
-
-// sortByPath sorts by Path
-func sortByPath(sl []domain.File) []domain.File {
-	sort.Slice(sl, func(i, j int) bool {
-		return sl[i].Path < sl[j].Path
-	})
-	return sl
-}
-
 // RemoveCLones calls file removal and corresponding storage data change
-func (srv *service) RemoveClones() error {
+func (srv *Service) RemoveClones() error {
 	err := srv.storage.GetClones()
 	if err != nil {
 		srv.slogger.Errorf("error geting clones, %v", err)
